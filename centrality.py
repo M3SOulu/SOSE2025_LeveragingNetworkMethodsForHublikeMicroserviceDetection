01b32881b4949388a5b3ee116770e2b17b3f02de
Alexander Bakhtin <alexander.bakhtin@oulu.fi>
2025-03-29 14:25:03 +0200

    remove: graphs with db refactor: move data to Metrics

--- centrality.py
+++ centrality.py
@@ -5,17 +5,16 @@ import networkx as nx
 import pandas as pd
 
 
-def compute_centrality(db: bool):
+def compute_centrality():
     all_dfs = []
-    d = "graph" if db else "graph_nodb"
-    for f in os.scandir(d):
+    for f in os.scandir("Raw"):
         with open(f, 'r') as fi:
             g = json.load(fi)
         G = nx.node_link_graph(g, edges="edges", nodes="nodes", name="name", source="sender", target="receiver",
                                multigraph=False, directed=True)
         G.remove_nodes_from(["user"])
-        name = f.name.replace("_gwcc.json", "") if db else f.name.replace("_gwcc_noDB.json", "")
-        graph_df = pd.DataFrame(columns=["MS_system", "node"])
+        name = f.name.replace("_gwcc_noDB.json", "")
+        graph_df = pd.DataFrame(columns=["MS_system", "Microservice"])
         for node in G.nodes:
             graph_df.loc[len(graph_df)] = [name, node]
         norm_degree_centrality = nx.degree_centrality(G)
@@ -26,29 +25,31 @@ def compute_centrality(db: bool):
         out_degree_centrality = {k: int(v * (len(G.nodes) - 1)) for k, v in norm_out_degree_centrality.items()}
         clustering_coefficient = nx.clustering(G)
 
-        graph_df[f"Degree Centrality"] = graph_df["node"].map(degree_centrality)
-        graph_df[f"Norm. Degree Centrality"] = graph_df["node"].map(norm_degree_centrality)
-        graph_df[f"In-degree Centrality"] = graph_df["node"].map(in_degree_centrality)
-        graph_df[f"Norm. In-degree Centrality"] = graph_df["node"].map(norm_in_degree_centrality)
-        graph_df[f"Out-degree Centrality"] = graph_df["node"].map(out_degree_centrality)
-        graph_df[f"Norm. Out-degree Centrality"] = graph_df["node"].map(norm_out_degree_centrality)
-        graph_df[f"Eigenvector Centrality"] = graph_df["node"].map(
+        graph_df[f"Degree Centrality"] = graph_df["Microservice"].map(degree_centrality)
+        graph_df[f"Norm. Degree Centrality"] = graph_df["Microservice"].map(norm_degree_centrality)
+        graph_df[f"In-degree Centrality"] = graph_df["Microservice"].map(in_degree_centrality)
+        graph_df[f"Norm. In-degree Centrality"] = graph_df["Microservice"].map(norm_in_degree_centrality)
+        graph_df[f"Out-degree Centrality"] = graph_df["Microservice"].map(out_degree_centrality)
+        graph_df[f"Norm. Out-degree Centrality"] = graph_df["Microservice"].map(norm_out_degree_centrality)
+        graph_df[f"Eigenvector Centrality"] = graph_df["Microservice"].map(
             nx.eigenvector_centrality(G, max_iter=1000))
-        graph_df[f"Betweenness Centrality"] = graph_df["node"].map(nx.betweenness_centrality(G))
-        graph_df[f"Closeness Centrality"] = graph_df["node"].map(nx.closeness_centrality(G))
-        graph_df[f"PageRank Centrality"] = graph_df["node"].map(nx.pagerank(G))
+        graph_df[f"Betweenness Centrality"] = graph_df["Microservice"].map(nx.betweenness_centrality(G))
+        graph_df[f"Closeness Centrality"] = graph_df["Microservice"].map(nx.closeness_centrality(G))
+        graph_df[f"PageRank Centrality"] = graph_df["Microservice"].map(nx.pagerank(G))
         hubs, authorities = nx.hits(G)
-        graph_df[f"Hub Score"] = graph_df["node"].map(hubs)
-        graph_df[f"Authority Score"] = graph_df["node"].map(authorities)
+        graph_df[f"Hub Score"] = graph_df["Microservice"].map(hubs)
+        graph_df[f"Authority Score"] = graph_df["Microservice"].map(authorities)
+
         G = nx.Graph(G)
-        graph_df[f"Subgraph Centrality"] = graph_df["node"].map(nx.subgraph_centrality(G))
-        graph_df["Clustering"] = graph_df["node"].map (clustering_coefficient)
+        graph_df[f"Subgraph Centrality"] = graph_df["Microservice"].map(nx.subgraph_centrality(G))
+        graph_df["Clustering"] = graph_df["Microservice"].map(clustering_coefficient)
 
         all_dfs.append(graph_df)
+
     all_dfs = sorted(all_dfs, key=lambda d: d["MS_system"].iloc[0].casefold())
     df_db = pd.concat(all_dfs)
     # df_db = df_db.sort_values(by="Eigenvector Centrality", ascending=False)
-    df_db.to_csv(f"metrics_centrality_db_{db}.csv", index=False, header=True)
+    df_db.to_csv(os.path.join("Metrics", "metrics_centrality.csv"), index=False, header=True)
 
     # Columns you want to calculate fractions for
     columns = ["Degree Centrality", "In-degree Centrality", "Out-degree Centrality"]
@@ -60,18 +61,18 @@ def compute_centrality(db: bool):
     proportions = pd.DataFrame(fractions_dict).fillna(0).reset_index()
     proportions.rename(columns={'index': 'degree'}, inplace=True)
 
-    proportions.to_csv(f"scale_free_test_{db}.csv", index=False, header=True)
+    proportions.to_csv(os.path.join("Metrics", "scale_free_proportion.csv"), index=False, header=True)
 
     # Create a new DataFrame where each column is the name sorted by that metric
     sorted_names_by_metric = {
-        col: df_db.sort_values(by=col, ascending=False)['node'].reset_index(drop=True)
-        for col in df_db.columns if col not in ["MS_system", "node", "Clustering"]
+        col: df_db.sort_values(by=col, ascending=False)["Microservice"].reset_index(drop=True)
+        for col in df_db.columns if col not in ["MS_system", "Microservice", "Clustering"]
     }
-    sorted_names_by_metric["Clustering"] =  df_db.sort_values(by="Clustering", ascending=True)['node'].reset_index(drop=True)
+    sorted_names_by_metric["Clustering"] =  df_db.sort_values(by="Clustering", ascending=True)["Microservice"].reset_index(drop=True)
     sorted_names_by_metric = pd.DataFrame(sorted_names_by_metric)
 
-    sorted_names_by_metric.to_csv(f"sorted_nodes_db_{db}.csv", index=False)
+    sorted_names_by_metric.to_csv(os.path.join("Metrics", "sorted_nodes.csv"), index=False)
 
 
-compute_centrality(True)
-compute_centrality(False)
+if __name__ == "__main__":
+    compute_centrality()
