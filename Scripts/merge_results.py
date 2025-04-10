b972c6e0419cacac080dfa78edd7a044ef77bd2f
Alexander Bakhtin <alexander.bakhtin@oulu.fi>
2025-04-10 11:35:57 +0300

    add: cluster coefficient results

--- Scripts/merge_results.py
+++ Scripts/merge_results.py
@@ -1,10 +1,25 @@
 import json
+import pandas as pd
 
 def main():
+    # Start by loading Kirkley
     with open("Results/Kirkley.json", 'r') as f:
         kirkley = json.load(f)
+    # Scale-free test failed, so no hubs for scale-free
     for system in kirkley:
         kirkley[system]["scale-free"] = []
+    # Insert results from the clustering scatterplots
+    clustering = pd.read_csv("Results/ClusteringRankAgreement.csv", delimiter=";")
+    for centrality in clustering.columns:
+        for item in clustering[centrality]:
+            if pd.isna(item):
+                continue
+            parts = item.split("_")
+            system = "_".join(parts[:-1])
+            service = parts[-1]
+            l = kirkley[system].setdefault(f"Clustering & {centrality}", [])
+            l.append(service)
+    # Save merged results
     with open("Results/Hubs.json", 'w') as f:
         json.dump(kirkley, f, indent=4)
 
