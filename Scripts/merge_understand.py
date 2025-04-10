e739feff4fcee9a6532a228c46576c367086e819
Alexander Bakhtin <alexander.bakhtin@oulu.fi>
2025-04-10 15:41:26 +0300

    refactor: change understand scripts

--- Scripts/merge_understand.py
+++ Scripts/merge_understand.py
@@ -1,6 +1,7 @@
 # Adapted from https://zenodo.org/records/14748464
 
 import os
+import json
 
 import pandas as pd
 
@@ -20,5 +21,41 @@ for project_dir in os.listdir("Raw/understand"):
     all_dfs.append(df)
 
 all_dfs = sorted(all_dfs, key=lambda d: d["MS_system"].iloc[0].casefold())
-df = pd.concat(all_dfs)
-df.to_csv("Metrics/metrics_understand.csv", index=False, header=True)
+understand = pd.concat(all_dfs)
+
+with open("Raw/package_map.json", 'r') as f:
+    PACKAGE_MAP = json.load(f)
+
+PACKAGES = set(PACKAGE_MAP.keys())
+SERVICES = set(PACKAGE_MAP.values())
+
+# Mappping of packages to services
+def map_packages(value: str):
+    for package, service in PACKAGE_MAP.items():
+        if value.startswith(package):
+            return service
+    return None
+
+
+# --- Understand metrics
+
+# Remove NaN columns
+understand = understand.dropna(axis=1, how='all')
+
+# Map Package to Microservice
+understand = understand.rename(columns={"Name": "Package"})
+understand["Microservice"] = understand["Package"].map(map_packages)
+# Remove rows that are not mapped to a service
+understand = understand.dropna(subset=["Microservice"])
+understand = understand.drop(columns=["Package"])
+
+
+# Group by microservice and sum all the metric columns
+understand = understand.groupby(["MS_system", 'Microservice']).sum().reset_index()
+understand = understand[["MS_system", "Microservice", "CountDeclMethodPublic"]]
+
+centrality = pd.read_csv("Metrics/metrics_centrality.csv")
+understand = pd.merge(understand, centrality, on=["MS_system", "Microservice"], how="inner")
+
+understand = understand.sort_values(by=["MS_system", "Microservice"])
+understand.to_csv("Metrics/metrics_merged.csv", index=False, header=True)
